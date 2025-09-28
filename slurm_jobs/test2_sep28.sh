5814cb545ce4b44e5a00212ad19003fd1980884e
Mingi Kang <mkang2@bowdoin.edu>
2025-09-28 16:17:00 -0400

    [Test] K Test for ConvNNAttention - K = 3, 5, 7, 9, 11, 13

---
+++ slurm_jobs/test2_sep28.sh
@@ -0,0 +1,38 @@
+#! /bin/bash 
+#SBATCH --nodes=1 
+#SBATCH --mem=64G
+#SBATCH -p gpu --gres=gpu:a100:1
+#SBATCH --cpus-per-task=4
+#SBATCH --job-name=VIT_sep28
+#SBATCH --time=500:00:00
+#SBATCH --output=slurm_out/%j.out
+#SBATCH --error=slurm_out/%j.err
+#SBATCH --mail-type=BEGIN,END,FAIL,TIME_LIMIT_80
+#SBATCH --mail-user=mkang2@bowdoin.edu
+
+cd /mnt/research/j.farias/mkang2/Convolutional-Nearest-Neighbor-Attention
+
+source activate mingi
+
+
+# VIT-Tiny Configuration 
+# patch_size: 16
+# num_layers:12
+# num_heads: 3
+# d_hidden: 192
+# d_mlp: 768
+
+# K Test for All Samples
+
+### CIFAR10 Experiments
+python main.py --layer ConvNNAttention --K 3 --sampling_type all --dataset cifar10 --output_dir ./Output/Sep28_K_Test/CIFAR10/ConvNNAttention_All_K3_s42 --num_epochs 80 --seed 42 --lr 1e-5 --lr_step 2 --lr_gamma 0.95
+
+python main.py --layer ConvNNAttention --K 5 --sampling_type all --dataset cifar10 --output_dir ./Output/Sep28_K_Test/CIFAR10/ConvNNAttention_All_K5_s42 --num_epochs 80 --seed 42 --lr 1e-5 --lr_step 2 --lr_gamma 0.95
+
+python main.py --layer ConvNNAttention --K 7 --sampling_type all --dataset cifar10 --output_dir ./Output/Sep28_K_Test/CIFAR10/ConvNNAttention_All_K7_s42 --num_epochs 80 --seed 42 --lr 1e-5 --lr_step 2 --lr_gamma 0.95
+
+python main.py --layer ConvNNAttention --K 9 --sampling_type all --dataset cifar10 --output_dir ./Output/Sep28_K_Test/CIFAR10/ConvNNAttention_All_K9_s42 --num_epochs 80 --seed 42 --lr 1e-5 --lr_step 2 --lr_gamma 0.95
+
+python main.py --layer ConvNNAttention --K 11 --sampling_type all --dataset cifar10 --output_dir ./Output/Sep28_K_Test/CIFAR10/ConvNNAttention_All_K11_s42 --num_epochs 80 --seed 42 --lr 1e-5 --lr_step 2 --lr_gamma 0.95
+
+python main.py --layer ConvNNAttention --K 13 --sampling_type all --dataset cifar10 --output_dir ./Output/Sep28_K_Test/CIFAR10/ConvNNAttention_All_K13_s42 --num_epochs 80 --seed 42 --lr 1e-5 --lr_step 2 --lr_gamma 0.95
