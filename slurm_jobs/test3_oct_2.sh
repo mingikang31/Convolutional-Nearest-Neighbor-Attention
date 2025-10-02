d71d96f61a9f237c25838de7c9d00857d3111901
Mingi Kang <mkang2@bowdoin.edu>
2025-10-02 17:41:37 -0400

    [Test] Only V Projection

--- slurm_jobs/test3_oct_2.sh
+++ slurm_jobs/test3_oct_2.sh
@@ -26,9 +26,11 @@ source activate mingi
 # Rand & Spatial: N = 32
 
 ### CIFAR10 Experiments
-python main.py --layer BranchConv --K 9 --kernel_size 9 --sampling_type all --dataset cifar10 --output_dir ./Output/Oct1-VIT-Tiny-Baseline/CIFAR10/BranchConv_ConvNNAttention_All_K9_br0500_s42_KQV --num_epochs 50 --seed 42 --lr 1e-5 --lr_step 2 --lr_gamma 0.95 --branch_ratio 0.5
+python main.py --layer ConvNNAttention --K 9 --sampling_type all --dataset cifar10 --output_dir ./Output/Oct1-VIT-Tiny-Baseline/CIFAR10/ConvNNAttention_All_Coord_K9_s42_V --num_epochs 50 --seed 42 --lr 1e-5 --lr_step 2 --lr_gamma 0.95 --coordinate_encoding
 
-python main.py --layer BranchAttention --K 9 --sampling_type all --dataset cifar10 --output_dir ./Output/Oct1-VIT-Tiny-Baseline/CIFAR10/BranchAttention_ConvNNAttention_All_K9_br0500_s42_KQV --num_epochs 50 --seed 42 --lr 1e-5 --lr_step 2 --lr_gamma 0.95 --branch_ratio 0.5
+python main.py --layer BranchConv --K 9 --kernel_size 9 --sampling_type all --dataset cifar10 --output_dir ./Output/Oct1-VIT-Tiny-Baseline/CIFAR10/BranchConv_ConvNNAttention_All_K9_br0500_s42_V --num_epochs 50 --seed 42 --lr 1e-5 --lr_step 2 --lr_gamma 0.95 --branch_ratio 0.5
+
+python main.py --layer BranchAttention --K 9 --sampling_type all --dataset cifar10 --output_dir ./Output/Oct1-VIT-Tiny-Baseline/CIFAR10/BranchAttention_ConvNNAttention_All_K9_br0500_s42_V --num_epochs 50 --seed 42 --lr 1e-5 --lr_step 2 --lr_gamma 0.95 --branch_ratio 0.5
 
 
 # TODO 
