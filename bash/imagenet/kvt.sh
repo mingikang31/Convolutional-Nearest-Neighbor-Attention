2c35305efafe26f7d0b5895e0ef93eec19d95629
mingikang31 <mkang2@bowdoin.edu>
2026-02-27 21:02:42 +0000

    [Jetstream] ConvNNAttention NH12 Rand K=9 N=32 results

--- bash/imagenet/kvt.sh
+++ bash/imagenet/kvt.sh
@@ -16,41 +16,13 @@ torchrun --nproc_per_node=4 \
         --model vit-base \
         --layer KvtAttention \
         --K 2 \
-        --convolution_type depthwise \
-        --softmax_topk_val \
-        --sampling_type all \
-        --num_samples -1 \
-        --num_heads 1 \
+        --num_heads 12 \
         --dataset imagenet1k \
         --data_path /home/exouser/Datasets \
         --compile \
         --use_amp \
         --device cuda \
-        --output_dir /home/exouser/Convolutional-Nearest-Neighbor-Attention/Output/ImageNet1K/ViT-Base-KvtAttention_NH1_K2/ \
-        --num_workers 16 \
-        --pin_memory \
-        --ddp \
-        --ddp_batch_size 320 || echo "K=4 training failed"
-
-
-torchrun --nproc_per_node=4 \
-         --rdzv_backend=c10d \
-         --rdzv_endpoint=localhost:$MASTER_PORT \
-         main.py \
-        --model vit-base \
-        --layer KvtAttention \
-        --K 4 \
-        --convolution_type depthwise \
-        --softmax_topk_val \
-        --sampling_type all \
-        --num_samples -1 \
-        --num_heads 1 \
-        --dataset imagenet1k \
-        --data_path /home/exouser/Datasets \
-        --compile \
-        --use_amp \
-        --device cuda \
-        --output_dir /home/exouser/Convolutional-Nearest-Neighbor-Attention/Output/ImageNet1K/ViT-Base-KvtAttention_NH1_K4/ \
+        --output_dir /home/exouser/Convolutional-Nearest-Neighbor-Attention/Output/ImageNet1K/ViT-Base-KvtAttention_NH12_K2/ \
         --num_workers 16 \
         --pin_memory \
         --ddp \
@@ -63,22 +35,38 @@ torchrun --nproc_per_node=4 \
          main.py \
         --model vit-base \
         --layer KvtAttention \
-        --K 9 \
-        --convolution_type depthwise \
-        --softmax_topk_val \
-        --sampling_type all \
-        --num_samples -1 \
-        --num_heads 1 \
+        --K 4 \
+        --num_heads 12 \
         --dataset imagenet1k \
         --data_path /home/exouser/Datasets \
         --compile \
         --use_amp \
         --device cuda \
-        --output_dir /home/exouser/Convolutional-Nearest-Neighbor-Attention/Output/ImageNet1K/ViT-Base-KvtAttention_NH1_K9/ \
+        --output_dir /home/exouser/Convolutional-Nearest-Neighbor-Attention/Output/ImageNet1K/ViT-Base-KvtAttention_NH12_K4/ \
         --num_workers 16 \
         --pin_memory \
         --ddp \
-        --ddp_batch_size 320 || echo "K=25 training failed"
+        --ddp_batch_size 320 || echo "K=4 training failed"
+
+
+torchrun --nproc_per_node=4 \
+         --rdzv_backend=c10d \
+         --rdzv_endpoint=localhost:$MASTER_PORT \
+         main.py \
+        --model vit-base \
+        --layer KvtAttention \
+        --K 9 \
+        --num_heads 12 \
+        --dataset imagenet1k \
+        --data_path /home/exouser/Datasets \
+        --compile \
+        --use_amp \
+        --device cuda \
+        --output_dir /home/exouser/Convolutional-Nearest-Neighbor-Attention/Output/ImageNet1K/ViT-Base-KvtAttention_NH12_K9/ \
+        --num_workers 16 \
+        --pin_memory \
+        --ddp \
+        --ddp_batch_size 320 || echo "K=9 training failed"
 
 torchrun --nproc_per_node=4 \
          --rdzv_backend=c10d \
@@ -87,20 +75,16 @@ torchrun --nproc_per_node=4 \
         --model vit-base \
         --layer KvtAttention \
         --K 16 \
-        --convolution_type depthwise \
-        --softmax_topk_val \
-        --sampling_type all \
-        --num_samples -1 \
-        --num_heads 1 \
+        --num_heads 12 \
         --dataset imagenet1k \
         --data_path /home/exouser/Datasets \
         --compile \
         --use_amp \
         --device cuda \
-        --output_dir /home/exouser/Convolutional-Nearest-Neighbor-Attention/Output/ImageNet1K/ViT-Base-KvtAttention_NH1_K16/ \
+        --output_dir /home/exouser/Convolutional-Nearest-Neighbor-Attention/Output/ImageNet1K/ViT-Base-KvtAttention_NH12_K16/ \
         --num_workers 16 \
         --pin_memory \
         --ddp \
-        --ddp_batch_size 320 || echo "K=25 training failed"
+        --ddp_batch_size 320 || echo "K=16 training failed"
 
 echo "All training completed"
